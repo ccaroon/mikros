ad8fa0244284e8a356521cfc7eea162af89bb64e
Craig N. Caroon <ccaroon@webassign.net>
2015-06-19 13:16:41 -0400

    * Put the finishing touches on the etch-a-sketch sketch.
    - changed background color to gray
    - added red frame/border
    - added some error handling so you can't draw off the screen
    - right button turns the screen off
    - reset no longer re-centers the cursor

--- etch_a_sketch/src/main.ino
+++ etch_a_sketch/src/main.ino
@@ -3,29 +3,41 @@
 #include "TinyScreen.h"
 #include "TinyJoystick.h"
 
+#define MIN_X 2 
+#define MAX_X TinyScreen::xMax-3
+#define MIN_Y 2
+#define MAX_Y TinyScreen::yMax-3
+
 TinyScreen   display  = TinyScreen(0);
 TinyJoystick joystick = TinyJoystick();
 
+bool    screenOn = true;
 uint8_t currentX = 0;
 uint8_t currentY = 0;
 
 void setup() {
-    Serial.begin(9600);
-    display.begin();
+    // Serial.begin(9600);
 
+    display.begin();
     // display.setFont(liberationSans_10ptFontInfo);
     // display.fontColor(,0x00);
     // display.print("etch-a-sketch");
+
+    // Center of display
+    currentX = 47;
+    currentY = 31;
+    display.goTo(currentX, currentY);
 
     reset();
 }
 
 void reset() {
-    display.drawRect(0,0,96,64,1,0xFF);
+    // Frame
+    display.drawRect(0,0,TinyScreen::xMax,TinyScreen::yMax,    0,0x03);
+    display.drawRect(1,1,TinyScreen::xMax-2,TinyScreen::yMax-2,0,0x03);
 
-    currentX = 48;
-    currentY = 32;
-    display.goTo(currentX, currentY);
+    // Background
+    display.drawRect(2,2,TinyScreen::xMax-4,TinyScreen::yMax-4,1,48,48,48);
 }
 
 void loop() {
@@ -36,26 +48,35 @@ void loop() {
     if (joystick.leftButtonPressed()) {
         reset();
     }
+    else if (joystick.rightButtonPressed()) {
+        if (screenOn) {
+            display.off();
+            screenOn = false;
+        }
+        else {
+            display.on();
+            screenOn = true;
+        }
+    }
     else {
-    // Left - Horizonal
-        Location left  = joystick.getLeftStick();
-        // Right - Vertical
-        Location right = joystick.getRightStick();
-
-        if (left.x > 200) {
+        // Left - Horizonal
+        Location left = joystick.getLeftStick();
+        if (left.x > 200 && currentX < MAX_X) {
             currentX++;
             cursorMoved = true;
         }
-        else if (left.x < -200) {
+        else if (left.x < -200 && currentX > MIN_X) {
             currentX--;
             cursorMoved = true;
         }
 
-        if (right.y > 200) {
+        // Right - Vertical
+        Location right = joystick.getRightStick();
+        if (right.y > 200 && currentY > MIN_Y) {
             currentY--;
             cursorMoved = true;
         }
-        else if (right.y < -200) {
+        else if (right.y < -200 && currentY < MAX_Y) {
             currentY++;
             cursorMoved = true;
         }
