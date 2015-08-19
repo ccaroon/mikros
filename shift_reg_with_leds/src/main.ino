1edc70125122a5c95930a6f6cf8d0078dec7ef52
Craig N. Caroon <craig@caroon.org>
2015-08-18 20:51:05 -0400

    * Changed code to work with 2 shift registers connected serially and two   sets of LEDs

--- shift_reg_with_leds/src/main.ino
+++ shift_reg_with_leds/src/main.ino
@@ -10,9 +10,13 @@ void openLatch() {
     digitalWrite(latchPin, HIGH);
 }
 
-void sendData(byte data) {
+void sendData(byte *data, uint8_t len) {
     closeLatch();
-    shiftOut(dataPin, clockPin, MSBFIRST, data);
+
+    for (uint8_t i = 0; i < len; i++) {
+        shiftOut(dataPin, clockPin, MSBFIRST, data[i]);
+    }
+    
     openLatch();
 }
 
@@ -22,41 +26,50 @@ void setup() {
     pinMode(clockPin, OUTPUT);
 }
 
-int led1 = 3;
-int led2 = 4;
-bool reverse = false;
 void loop() {
-    byte data = 0;
+    lightInSequence();
+}
 
-    bitWrite(data, led1, HIGH);
-    bitWrite(data, led2, HIGH);
-    sendData(data);
-    delay(100);
-    bitWrite(data, led1, LOW);
-    bitWrite(data, led2, LOW);
-    sendData(data);
+int greenLed = 0;
+int blueLed  = 7;
+void lightInSequence() {
+    byte greenData = 0;
+    byte blueData  = 0;
+    byte data[2];
 
-    if (reverse) {
-        if (led1 == 3 && led2 == 4) {
-            reverse = false;
-            led1--;
-            led2++;
-        }
-        else {
-            led1++;
-            led2--;
-        }
-    }
-    else {
-        if (led1 == 0 && led2 == 7) {
-            reverse = true;
-            led1++;
-            led2--;
-        }
-        else {
-            led1--;
-            led2++;
-        }
+    bitWrite(greenData, greenLed, HIGH);
+    bitWrite(blueData,  blueLed, HIGH);
+    
+    data[0] = blueData;
+    data[1] = greenData;
+    sendData(data, 2);
+
+    delay(750);
+
+    greenLed++;
+    if (greenLed == 8) {
+        greenLed = 0;
     }
 
+    blueLed--;
+    if (blueLed < 0) {
+        blueLed = 7;
+    }
+}
+
+void helloWorld() {
+  // count from 0 to 255 and display the number 
+  // on the LEDs
+  for (int numberToDisplay = 0; numberToDisplay < 256; numberToDisplay++) {
+    // take the latchPin low so 
+    // the LEDs don't change while you're sending in bits:
+    digitalWrite(latchPin, LOW);
+    // shift out the bits:
+    shiftOut(dataPin, clockPin, MSBFIRST, numberToDisplay);  
+
+    //take the latch pin high so the LEDs will light up:
+    digitalWrite(latchPin, HIGH);
+    // pause before next value:
+    delay(500);
+  }
 }
