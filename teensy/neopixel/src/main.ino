ea43887cb8e6c3ceada5b300db538b0d4dc120ea
Craig N. Caroon <craig@caroon.org>
2016-12-08 19:57:18 -0500

    Adding some teensy neopixel code

---
+++ teensy/neopixel/src/main.ino
@@ -0,0 +1,165 @@
+// candle for Adafruit NeoPixel
+// 1 pixel version
+// by Tim Bartlett, December 2013
+
+// current settings for 5v Trinket
+
+#include <Adafruit_NeoPixel.h>
+#define PIN 7
+
+// color variables: mix RGB (0-255) for desired yellow
+int redPx = 255;
+int grnHigh = 135; // 110-120 for 5v, 135 for 3.3v
+int bluePx = 15;   // 10 for 5v, 15 for 3.3v
+
+// animation time variables, with recommendations
+int burnDepth = 10;    // 10 for 5v, 14 for 3.3v -- how much green dips below
+                       // grnHigh for normal burn -
+int flutterDepth = 25; // 25 for 5v, 30 for 3.3v -- maximum dip for flutter
+int cycleTime = 120;   // 120 -- duration of one dip in milliseconds
+
+// pay no attention to that man behind the curtain
+int fDelay;
+int fRep;
+int flickerDepth;
+int burnDelay;
+int burnLow;
+int flickDelay;
+int flickLow;
+int flutDelay;
+int flutLow;
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(1, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+    // candle_setup();
+    misc_setup();
+    strip.begin();
+    strip.show();
+}
+
+void loop() {
+    // candle_loop();
+    misc_loop();
+}
+
+// ------------------- Misc Stuff
+void misc_setup() {
+}
+
+void misc_loop() {
+    int dtime = 3000;
+    byte i = 255;
+
+    // black
+    strip.setPixelColor(0, 0, 0, 0);
+    strip.show();
+    delay(dtime);
+
+    // yellow
+    strip.setPixelColor(0, i, i, 0);
+    strip.show();
+    delay(dtime);
+
+    // red
+    strip.setPixelColor(0, i, 0, 0);
+    strip.show();
+    delay(dtime);
+
+    // blue
+    strip.setPixelColor(0, 0, i, 0);
+    strip.show();
+    delay(dtime);
+}
+
+// ------------------- Candle Stuff
+void candle_setup() {
+    flickerDepth = (burnDepth + flutterDepth) / 2.4;
+    burnLow = grnHigh - burnDepth;
+    burnDelay = (cycleTime / 2) / burnDepth;
+    flickLow = grnHigh - flickerDepth;
+    flickDelay = (cycleTime / 2) / flickerDepth;
+    flutLow = grnHigh - flutterDepth;
+    flutDelay = ((cycleTime / 2) / flutterDepth);
+}
+
+// In loop, call CANDLE STATES, with duration in seconds
+// 1. on() = solid yellow
+// 2. burn() = candle is burning normally, flickering slightly
+// 3. flicker() = candle flickers noticably
+// 4. flutter() = the candle needs air!
+
+void candle_loop() {
+    flutter(10);
+    burn(10);
+    flicker(5);
+    burn(8);
+    flutter(6);
+    burn(3);
+    on(10);
+    burn(10);
+    flicker(10);
+}
+
+// basic fire funciton - not called in main loop
+void fire(int grnLow) {
+    for (int grnPx = grnHigh; grnPx > grnLow; grnPx--) {
+        strip.setPixelColor(0, redPx, grnPx, bluePx);
+        strip.show();
+        delay(fDelay);
+    }
+    for (int grnPx = grnLow; grnPx < grnHigh; grnPx++) {
+        strip.setPixelColor(0, redPx, grnPx, bluePx);
+        strip.show();
+        delay(fDelay);
+    }
+}
+
+// fire animation
+void on(int f) {
+    fRep = f * 1000;
+    int grnPx = grnHigh - 5;
+    strip.setPixelColor(0, redPx, grnPx, bluePx);
+    strip.show();
+    delay(fRep);
+}
+
+void burn(int f) {
+    fRep = f * 8;
+    fDelay = burnDelay;
+    for (int var = 0; var < fRep; var++) {
+        fire(burnLow);
+    }
+}
+
+void flicker(int f) {
+    fRep = f * 8;
+    fDelay = burnDelay;
+    fire(burnLow);
+    fDelay = flickDelay;
+    for (int var = 0; var < fRep; var++) {
+        fire(flickLow);
+    }
+    fDelay = burnDelay;
+    fire(burnLow);
+    fire(burnLow);
+    fire(burnLow);
+}
+
+void flutter(int f) {
+    fRep = f * 8;
+    fDelay = burnDelay;
+    fire(burnLow);
+    fDelay = flickDelay;
+    fire(flickLow);
+    fDelay = flutDelay;
+    for (int var = 0; var < fRep; var++) {
+        fire(flutLow);
+    }
+    fDelay = flickDelay;
+    fire(flickLow);
+    fire(flickLow);
+    fDelay = burnDelay;
+    fire(burnLow);
+    fire(burnLow);
+}
