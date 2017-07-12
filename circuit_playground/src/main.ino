adb4cc90763dd87bf2d07e2c74c291850de2933d
Craig N. Caroon <craig@caroon.org>
2017-07-11 20:49:51 -0400

    * circuit_playground main.ino....still just playing around

--- circuit_playground/src/main.ino
+++ circuit_playground/src/main.ino
@@ -1,8 +1,20 @@
+#include <stdio.h>
+
 #include <Adafruit_CircuitPlayground.h>
+#define TILT_COLOR 0x00FF77
+
+void setAllColor(uint32_t color) {
+    for (uint8_t i = 0; i < 10; i++) {
+        CircuitPlayground.setPixelColor(i, color);
+    }
+}
 
 void setup() {
+    Serial.begin(9600);
     CircuitPlayground.begin();
     CircuitPlayground.clearPixels();
+
+    setAllColor(TILT_COLOR);
 }
 
 void colorLoop() {
@@ -18,6 +30,69 @@ void colorLoop() {
     }
 }
 
+void displayTemp() {
+    float tempF = CircuitPlayground.temperatureF();
+
+    Serial.println(tempF);
+
+    if (tempF >= 90.0) {
+        CircuitPlayground.setPixelColor(2, 255, 0, 0);
+    } else {
+        CircuitPlayground.setPixelColor(2, 0, 255, 0);
+    }
+
+    delay(1000);
+}
+
+void tilt() {
+
+    float x = CircuitPlayground.motionX();
+    float y = CircuitPlayground.motionY();
+    float z = CircuitPlayground.motionZ();
+
+    if (x > -5 && x < 5 && y > -5 && y < 5) {
+        setAllColor(TILT_COLOR);
+    } else if (x <= -5) {
+        CircuitPlayground.clearPixels();
+        CircuitPlayground.setPixelColor(0, TILT_COLOR);
+        CircuitPlayground.setPixelColor(1, TILT_COLOR);
+        CircuitPlayground.setPixelColor(8, TILT_COLOR);
+        CircuitPlayground.setPixelColor(9, TILT_COLOR);
+    } else if (x >= 5) {
+        CircuitPlayground.clearPixels();
+        CircuitPlayground.setPixelColor(3, TILT_COLOR);
+        CircuitPlayground.setPixelColor(4, TILT_COLOR);
+        CircuitPlayground.setPixelColor(5, TILT_COLOR);
+        CircuitPlayground.setPixelColor(6, TILT_COLOR);
+    } else if (y <= -5) {
+        CircuitPlayground.clearPixels();
+        CircuitPlayground.setPixelColor(0, TILT_COLOR);
+        CircuitPlayground.setPixelColor(1, TILT_COLOR);
+        CircuitPlayground.setPixelColor(2, TILT_COLOR);
+        CircuitPlayground.setPixelColor(3, TILT_COLOR);
+        CircuitPlayground.setPixelColor(4, TILT_COLOR);
+    } else if (y >= 5) {
+        CircuitPlayground.clearPixels();
+        CircuitPlayground.setPixelColor(5, TILT_COLOR);
+        CircuitPlayground.setPixelColor(6, TILT_COLOR);
+        CircuitPlayground.setPixelColor(7, TILT_COLOR);
+        CircuitPlayground.setPixelColor(8, TILT_COLOR);
+        CircuitPlayground.setPixelColor(9, TILT_COLOR);
+    }
+
+    Serial.println("----------------------------------------");
+    Serial.print("x: ");
+    Serial.println(x);
+    Serial.print("y: ");
+    Serial.println(y);
+    Serial.print("z: ");
+    Serial.println(z);
+
+    delay(100);
+}
+
 void loop() {
     // colorLoop();
+    // displayTemp();
+    tilt();
 }
