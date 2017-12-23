dfe92c7daae0a9650638529101d716d84de30c5f
Craig N. Caroon <craig@caroon.org>
2017-12-23 15:20:45 -0500

    * main.py
    - more tweaking
    - more animations

--- circuit_python/cp_express/main.py
+++ circuit_python/cp_express/main.py
@@ -3,6 +3,18 @@ import random
 from adafruit_circuitplayground.express import cpx
 
 ################################################################################
+RED    = (255,0,0)
+ORANGE = (255,128,0)
+YELLOW = (255,255,0)
+GREEN  = (0,255,0)
+BLUE   = (0,0,255)
+INDIGO = (255,0,128)
+VIOLET = (128,0,255)
+
+PRIMARY_COLORS = [
+    RED,ORANGE,YELLOW,GREEN,BLUE,INDIGO,VIOLET
+]
+################################################################################
 # Helper Functions
 ################################################################################
 def set_color(color = (0,0,0)):
@@ -24,22 +36,23 @@ def led_on(i, color):
 ################################################################################
 # Pixel "Animations"
 ################################################################################
-def blink(color):
+def blink(color = (0,42,142)):
     '''Blink all the pixels on/off in the given color'''
     clear_all()
     time.sleep(1.0)
     set_color(color)
     time.sleep(1.0)
 
-def twinkle(color = (128,128,0)):
+def twinkle(count = 1, color = (128,128,0), wait = 0.25):
     '''Turn random pixels on & off in the given color giving the impression of
     them twinkling'''
-    i = random.randint(0,9);
 
     clear_all()
-    cpx.pixels[i] = color
+    for i in range(count):
+        p = random.randint(0,9)
+        cpx.pixels[p] = color
 
-    time.sleep(0.20)
+    time.sleep(wait)
 
 def progress(duration, color, clear = True):
     '''Light up the pixels in order with the given color over the give duration'''
@@ -84,15 +97,108 @@ def spinner(duration, color, clear = True):
         time.sleep(wait)
 
     clear_all()
+
+def alt_colors(cycle, color1, color2):
+    '''Alternate two colors on the even/odd pixels'''
+    evn_color = color1
+    odd_color = color2
+    if cycle % 2 == 0:
+        evn_color = color2
+        odd_color = color1
+
+    for i in range(10):
+        if i % 2 == 0:
+            led_on(i, evn_color)
+        else:
+            led_on(i, odd_color)
+
+    time.sleep(1.0)
+
+def random_color():
+    '''Set all pixels each to a random color'''
+    for i in range(10):
+        r = random.randint(0,255)
+        g = random.randint(0,255)
+        b = random.randint(0,255)
+        led_on(i, (r,g,b))
+
+    time.sleep(0.5)
+
+def primary_colors():
+        '''Set all pixels each to a random PRIMARY_COLOR'''
+    color_count = len(PRIMARY_COLORS)
+    for i in range(10):
+        color_index = random.randint(0, color_count-1)
+        led_on(i, PRIMARY_COLORS[color_index])
+
+    time.sleep(0.5)
+
+# def wheel(pos):
+#     # Input a value 0 to 255 to get a color value.
+#     # The colours are a transition r - g - b - back to r.
+#     # return ((0,124,255))
+#     if (pos < 0):
+#         return (0, 0, 0)
+#     if (pos > 255):
+#         return (0, 0, 0)
+#     if (pos < 85):
+#         return (int(pos * 3), int(255 - (pos*3)), 0)
+#     elif (pos < 170):
+#         pos -= 85
+#         return (int(255 - pos*3), 0, int(pos*3))
+#     else:
+#         pos -= 170
+#         return (0, int(pos*3), int(255 - pos*3))
+#
+# # i = 0
+# def color_wheel():
+#     for p in range(10):
+#         # led_on(p, wheel(p))
+#         # idx = int ((p * 256 / 10) + i)
+#         # cpx.pixels[p] = wheel(idx & 255)
+#         led_on(p, wheel(p & 255))
+#
+#     # i = (i+1) % 256  # run from 0 to 255
 ################################################################################
-def do_loop():
-    pass
+ACTION_INDEX = 0
+ACTIONS = [twinkle,blink]
+color = (64,64,128)
+################################################################################
+def do_loop(count):
+
+    # if cpx.button_a:
+        # color = (0,128,128)
+        # pass
+        # ACTION_INDEX = 0
+        # ACTION_INDEX += 1
+        # if ACTION_INDEX > len(ACTIONS):
+        #     ACTION_INDEX = 0
+        # current_action = ACTIONS[ACTION_INDEX]
+
+    # if cpx.button_b:
+        # pass
+        # ACTION_INDEX = 1
+
+    if cpx.switch:
+        twinkle(3,color,0.20)
+    else:
+        # blink(color)
+        # color_wheel()
+        # alt_colors(count, (128,0,0),(0,128,0))
+        # random_color()
+        primary_colors()
+
+    # ACTIONS[ACTION_INDEX]()
+
 ################################################################################
 # Main
 ################################################################################
+count = 0
 while True:
-    # blink((0,255,128))
-    # twinkle()
-    # spinner(0.75, (128,0,255))
-    progress(2.0, (255,0,128))
-    # time.sleep(1.0)
+    do_loop(count)
+    count += 1
+
+
+
+
+################################################################################
