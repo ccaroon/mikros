b227cec47fdf3bb2bcbb013f2d6eac2859994079
Craig N. Caroon <craig@caroon.org>
2017-12-23 11:44:08 -0500

    * main.py
    - add more animations
    - add more docstrings

--- circuit_python/cp_express/main.py
+++ circuit_python/cp_express/main.py
@@ -22,13 +22,18 @@ def led_on(i, color):
     '''Turn individual pixel on with the given color'''
     cpx.pixels[i] = color
 ################################################################################
-def blink():
+# Pixel "Animations"
+################################################################################
+def blink(color):
+    '''Blink all the pixels on/off in the given color'''
     clear_all()
     time.sleep(1.0)
-    set_color((0,0,255))
+    set_color(color)
     time.sleep(1.0)
 
 def twinkle(color = (128,128,0)):
+    '''Turn random pixels on & off in the given color giving the impression of
+    them twinkling'''
     i = random.randint(0,9);
 
     clear_all()
@@ -36,7 +41,23 @@ def twinkle(color = (128,128,0)):
 
     time.sleep(0.20)
 
+def progress(duration, color, clear = True):
+    '''Light up the pixels in order with the given color over the give duration'''
+    wait = duration / 10
+
+    if clear:
+        clear_all()
+
+    for i in range(10):
+        p = i+1
+        led_off(p - 1)
+        time.sleep(wait)
+        led_on(i, color)
+    led_off(9)
+
 def spinner(duration, color, clear = True):
+    '''Create a spinner of the given color that takes the given duration to make
+    one complete cycle'''
     wait = duration / 10;
 
     if clear:
@@ -63,11 +84,15 @@ def spinner(duration, color, clear = True):
         time.sleep(wait)
 
     clear_all()
-
 ################################################################################
-
+def do_loop():
+    pass
+################################################################################
+# Main
+################################################################################
 while True:
-    # blink()
+    # blink((0,255,128))
     # twinkle()
-    spinner(0.75, (0,25,0))
+    # spinner(0.75, (128,0,255))
+    progress(2.0, (255,0,128))
     # time.sleep(1.0)
