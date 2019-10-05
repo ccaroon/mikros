6c118a8c549fa5747801da6ea09ef67d216653ff
Craig N. Caroon <craig@caroon.org>
2019-10-05 11:46:33 -0400

    micropython - boot for esp8266 pip install issue fixes - added lib/file_utils.py

--- micropython/boots/esp8266.py
+++ micropython/boots/esp8266.py
@@ -3,7 +3,10 @@ import uos
 
 from chronos import Chronos
 
-if "logging.py" not in uos.listdir('/lib'):
+if "lib" not in uos.listdir():
+    uos.mkdir("lib")
+
+if "logging.py" not in uos.listdir('./lib'):
     upip.install("logging")
 
 Chronos.sync()
