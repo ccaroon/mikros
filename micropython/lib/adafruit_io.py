222a1e74e796375e32b37a18e49b966b7fa69017
Craig N. Caroon <craig@caroon.org>
2019-11-29 10:51:35 -0500

    * added get_data to adafruit_id

--- micropython/lib/adafruit_io.py
+++ micropython/lib/adafruit_io.py
@@ -10,6 +10,34 @@ class AdafruitIO:
 
         self.__group_name = group
 
+    def get_data(self, feed, **kwargs):
+        feed_name = "%s.%s" % (self.__group_name, feed)
+
+        limit = kwargs.get('limit', 1)
+        fields = ['value']
+        fields.extend(kwargs.get('fields', []))
+        include = ','.join(fields)
+        url = F"{AdafruitIO.BASE_URL}/{self.__username}/feeds/{feed_name}/data?limit={limit}&include={include}"
+        headers = {'X-AIO-Key': self.__key}
+
+        resp = requests.get(url, headers=headers)
+
+        if resp.status_code == 200:
+            output = {
+                "success": True,
+                "results" : resp.json()
+            }
+        else:
+            output = {
+                "success": False,
+                "code": resp.status_code,
+                "feed": feed_name,
+                "msg": resp.json()['error']
+            }
+
+        return (output)
+
+
     # TODO: use kwargs for non-required feed data fields
     def publish_data(self, feed, value, **kwargs):
         dry_run = kwargs.get('dry_run', False)
@@ -31,7 +59,7 @@ class AdafruitIO:
                 }
             }
         else:
-            resp = urequests.post(url, headers=headers,json=data)
+            resp = urequests.post(url, headers=headers, json=data)
 
             if resp.status_code == 200:
                 output = {
