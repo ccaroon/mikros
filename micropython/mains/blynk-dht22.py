525d1e6a43bcc2a1c014e2a5331804c3f36c5c09
Craig N. Caroon <craig@caroon.org>
2019-12-21 14:17:02 -0500

    * Got blynk-dh22.py working

--- micropython/mains/blynk-dht22.py
+++ micropython/mains/blynk-dht22.py
@@ -4,17 +4,8 @@ import utime
 import blynklib
 import dht
 
-print("--- Connecting to Blynk...")
-# BLINK
-# BLYNK_AUTH = 'kzV-4_HpLd3urNbZAAMggixDxFbcyPvM'
-
-# WS
-BLYNK_AUTH = 'ePczqByz8YHSJJnYJDow8Dgb6K0TIAi-'
+BLYNK_AUTH = 'UaAufOIriuP0hCw_XltDU1x8sRxTlKMM'
 blynk = blynklib.Blynk(BLYNK_AUTH)
-
-T_COLOR = '#f5b041'
-H_COLOR = '#85c1e9'
-ERR_COLOR = '#444444'
 
 T_VPIN = 1
 H_VPIN = 2
@@ -23,29 +14,25 @@ dht22 = dht.DHT22(Pin(14, Pin.IN, Pin.PULL_UP))
 
 @blynk.handle_event('read V{}'.format(T_VPIN))
 def read_handler(vpin):
-    temperature = 0.0
+    tempC = 0.0
+    tempF = 0.0
     humidity = 0.0
 
     # read sensor data
     try:
         dht22.measure()
-        temperature = dht22.temperature()
-        humidity = dht22.humidity()
+        tempC = dht22.temperature()
+        tempF = round(tempC * 9/5 + 32)
+        humidity = round(dht22.humidity())
     except OSError as o_err:
         print("Unable to get DHT22 sensor data: '{}'".format(o_err))
 
     # change widget values and colors according read results
-    if temperature != 0.0 and humidity != 0.0:
-        blynk.set_property(T_VPIN, 'color', T_COLOR)
-        blynk.set_property(H_VPIN, 'color', H_COLOR)
-        blynk.virtual_write(T_VPIN, temperature)
+    if tempF != 0.0 and humidity != 0.0:
+        blynk.virtual_write(T_VPIN, tempF)
         blynk.virtual_write(H_VPIN, humidity)
-        print(temperature, humidity)
-    else:
-        # show widgets aka 'disabled' that mean we had errors during read sensor operation
-        blynk.set_property(T_VPIN, 'color', ERR_COLOR)
-        blynk.set_property(H_VPIN, 'color', ERR_COLOR)
+        print("%02dF -- %02d%%" % (tempF, humidity))
 
-
-while True:
-    blynk.run()
+if __name__ == "__main__":
+    while True:
+        blynk.run()
